28ec53bf4b54dba6edd9721fab4706a1f6602b2f
withewolf-dev <itskashyap26@gmail.com>
2022-01-17 19:53:15 +0530

    backend

--- Document.js
+++ Document.js
@@ -3,6 +3,8 @@ const { Schema, model } = require("mongoose");
 const Document = new Schema({
   _id: String,
   data: Object,
+  userId: String,
+  title: String,
 });
 
 module.exports = model("Document", Document);
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const bodyParser = require("body-parser");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const Document = require("./Document");
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parses the text as json
+app.use(bodyParser.json());
 
 mongoose.connect(
   "mongodb+srv://googled:googledpw@cluster0.hgy4n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
@@ -19,29 +28,49 @@ const io = new Server(server, {
 const defaultValue = "";
 io.on("connection", (socket) => {
   console.log("a user connected");
-  socket.on("get-document", async (id) => {
-    const document = await findOrCreateDocument(id);
+  socket.on("get-document", async ({ id, userId, title }) => {
+    const document = await findOrCreateDocument({ id, userId, title });
     socket.join(id);
-    console.log(id);
-    socket.emit("load-document", document.data);
+    console.log(document.title, "document");
+    socket.emit("load-document", document);
     socket.on("send-changes", (delta) => {
       console.log("message: " + JSON.stringify(delta.ops));
       socket.broadcast.to(id).emit("receive-changes", delta);
     });
-    socket.on("save-document", async (data) => {
-      await Document.findByIdAndUpdate(id, { data });
+    socket.on("save-document", async ({ data, title }) => {
+      //console.log(data, title, "data");
+      await Document.findByIdAndUpdate(id, { data, title });
     });
   });
 });
 
-async function findOrCreateDocument(id) {
+async function findOrCreateDocument({ id, userId, title }) {
   if (id == null) return;
 
   const document = await Document.findById(id);
   if (document) return document;
-  return await Document.create({ _id: id, data: defaultValue });
+  return await Document.create({ _id: id, data: defaultValue, userId, title });
 }
 
+//get document
+app.get("/doc/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  if (userId == null) return;
+
+  const docs = await Document.find({ userId: userId });
+  res.json(docs);
+});
+
+app.delete("/doc/:id", async (req, res) => {
+  const { id } = req.params;
+  if (id == null) return;
+
+  await Document.findByIdAndDelete(id);
+
+  res.json({ status: "ok" });
+});
+
 server.listen(3001, () => {
-  console.log("listening on *:3000");
+  console.log("listening on *:3001");
 });
